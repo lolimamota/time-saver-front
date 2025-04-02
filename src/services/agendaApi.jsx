299b62f427d6da06740fff00df7175870340e623
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-04-02 17:47:29 -0300

    FIX: nome da pasta patterns para scss, separando a lógica da API em um componente

---
+++ src/services/agendaApi.jsx
@@ -0,0 +1,10 @@
+import {use, useState} from "react";
+import axios from "axios";
+
+export default function agendamento(){
+    const [dia, setDia] = useState("");
+    const [horario, setHorario] = useState("");
+    const [nome, setNome] = useState("");
+    const [convenio, setConvenio] = useState("");
+    const [especialidade, setEspecialidade] = useState("");
+}
