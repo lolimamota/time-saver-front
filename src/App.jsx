f68dc20c7d6ec219c1f19f9f67e4bc61d51906c0
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-04-02 13:57:28 -0300

    ADD: começo da função do APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React from 'react';
+import './global.scss';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+// -------------- IMPORT DE ROTAS E COMPONENTS --------------
+import novo_agendamento from './components/agendamento/post_agenda.jsx';
+import ver_agendamento from './components/verificar/get_agenda.jsx';
+
 
 function App() {
   return (
