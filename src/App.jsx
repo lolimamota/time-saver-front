c82027e3d55e6692b61d4a876fb618ecd274264e
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-04-02 14:36:57 -0300

    ADD: Rotas criadas e linkadas

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,36 @@ import './global.scss';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 // -------------- IMPORT DE ROTAS E COMPONENTS --------------
-import novo_agendamento from './components/agendamento/post_agenda.jsx';
-import ver_agendamento from './components/verificar/get_agenda.jsx';
+import Novo_agendamento from './components/agendamento/post_agenda.jsx';
+import Ver_agendamento from './components/verificar/get_agenda.jsx';
 
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <header>
+        <BrowserRouter>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/novo_agendamento">
+                  Novo agendamento
+                </Link>
+              </li>
+              <li>
+                <Link to="/ver_agendamento">
+                  Agendamentos anteriores
+                </Link>
+              </li>
+            </ul>
+            <Routes>
+              <Route path="/novo_agendamento" element={<Novo_agendamento />} />
+              <Route path="/ver_agendamento" element={<Ver_agendamento />} />
+            </Routes>
+          </nav>
+        </BrowserRouter>
+      </header>
+    </>
   )
 }
 
