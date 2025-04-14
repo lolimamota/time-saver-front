e20a82d4f446f13eb288b6422ca997dbe6f0ae25
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-04-14 12:35:04 -0300

    FIX: Header alinhada e com direcionamento

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,11 +5,21 @@ import React from 'react'
 import '../../global.scss';
 import S from '../../scss/components/header/header.module.scss';
 
+//IMPORT DE IMAGENS
+import Logo from '../../assets/logo.png';
+
 
 function header() {
   return (
     <header className={S.headerStyle}>
-      <h1>Time Saver</h1>
+      <a href="/">
+        <div>
+          <img src={Logo} alt="Imagem de uma engrenagem com um verificado verde" />
+          <h1>
+            Time Saver
+          </h1>
+        </div>
+      </a>
     </header>
   )
 }
