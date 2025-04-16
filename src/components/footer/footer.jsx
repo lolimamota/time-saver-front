96f0829586d1151639875407bed9c788cd39e746
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-04-16 10:32:05 -0300

    ADD: imagens de navegação, grid layout e inicio do alinhamento dos itens

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,26 +2,43 @@
 import React from 'react'
 
 //IMPORTS DE IMAGEM
+import Insta from '../../assets/insta.png';
+import Wpp from '../../assets/wpp.png';
+import Mail from '../../assets/mail.png';
+import Site from '../../assets/site.png';
+
 //IMPORTS DE ESTILO
 import S from '../../scss/components/footer/footer.module.scss'
 
 function footer() {
   return (
     <footer className={S.footerStyle}>
-      <p>Design criado para o processo seletivo Time Saver - 2025</p>
-      <p>Todos os direitos reservados</p>
-      <ul>
+      <ul className={S.firstColumn}>
         <li>
-          <a href="https://www.instagram.com/timesaverbr/" target='_blank'>Imagem do instagram</a>
+          <a href="https://timesaver.com.br/m/" target='_blank'>
+            <img src={Site} alt="Icone circular representando o planeta indicando o link para o site da TimeSaver" />
+          </a>
+        </li>
+        <li >
+          <a href="https://www.instagram.com/timesaverbr/" target='_blank'>
+            <img src={Insta} alt="logo do Instagram" />
+          </a>
+        </li>
+      </ul>
+      <div className={S.secondColumn}>
+        <p>Design criado para o processo seletivo Time Saver - 2025</p>
+        <p>Todos os direitos reservados</p>
+      </div>
+      <ul className={S.thirdColumn}>
+        <li>
+          <a href="https://api.whatsapp.com/send/?phone=5541998040107&text&type=phone_number&app_absent=0" target='_blank'>
+            <img src={Wpp} alt="Logo do WhatsApp" />
+          </a>
         </li>
         <li>
-          <a href="https://timesaver.com.br/m/">Imagem do site</a>
-        </li>
-        <li>
-          <a href="https://api.whatsapp.com/send/?phone=5541998040107&text&type=phone_number&app_absent=0">Imagem do Wpp</a>
-        </li>
-        <li>
-          <a href="mailto:contato@timesaver.com.br">Imagem do email</a>
+          <a href="mailto:contato@timesaver.com.br">
+            <img src={Mail} alt="Icone de uma carta fechada indicando acesso ao envio de um email para a TimeSaver" />
+          </a>
         </li>
       </ul>
 
